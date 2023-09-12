cc2e4f587a78c4f29f4bd7a5ba37063af2af5362
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-09-12 11:15:42 +0530

    Study-12thSept2023][CollectionDemo] Updated  URL patterns of all resources.
    Study-12thSept2023][CollectionDemo] Updated  URL patterns of all resources.

--- collection-demo/src/main/java/com/learn/java/collection/service/SetDemoService.java
+++ collection-demo/src/main/java/com/learn/java/collection/service/SetDemoService.java
@@ -1,13 +1,48 @@
 package com.learn.java.collection.service;
 
+import java.util.HashSet;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SetDemoService {
 
+	/*
+	 * 0.) Underlying Data Structure: Hash Table
+	 * 
+	 * 1.) Insertion Order Not Preserved (as per hashing logic)
+	 * 2.) Duplicates Not Allowed
+	 * 3.) NULL insertion is possible
+	 * 4.) No AutoSorting
+	 * 
+	 * 
+	 * */
 	public void hashSetDemo() {
-		// TODO Auto-generated method stub
-		
+		HashSet<String> hashSet = new HashSet<>();
+		hashSet.add("A");
+		hashSet.add("B");
+		hashSet.add("D");
+		hashSet.add("C");
+		hashSet.add("F");
+		hashSet.add("W");
+		hashSet.add("Z");
+		hashSet.add("S");
+		hashSet.add("R");
+		hashSet.add("A");
+		System.out.println("Added Hash Set: "+hashSet);
+		hashSet.add(null);
+		System.out.println("Hash Set after adding null: "+hashSet);
+		hashSet.add(null);
+		System.out.println("Hash Set again adding null: "+hashSet);
+		hashSet.add("R");
+		System.out.println("Hash Set again adding R: "+hashSet);
+		System.out.println("Hash Set Contains R? => "+hashSet.contains("R"));
+		System.out.println("Hash Set Contains null? => "+hashSet.contains(null));		
+		System.out.println("Hash Set removing R by index: "+hashSet.remove(hashSet.size()-1));
+		System.out.println(hashSet);
+		System.out.println("Hash Set removing R by Object: "+hashSet.remove("R"));
+		System.out.println(hashSet);
+
 	}
 
 	public void linkedHashSetDemo() {
