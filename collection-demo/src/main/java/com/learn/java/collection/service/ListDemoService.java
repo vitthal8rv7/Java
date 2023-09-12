3fe62680cb979149db66bb66de6cada30b8d1526
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-09-12 10:55:28 +0530

    Study-12thSept2023][CollectionDemo] Added code to test ArrayList.
    Study-12thSept2023][CollectionDemo] Added code to test ArrayList.

--- collection-demo/src/main/java/com/learn/java/collection/service/ListDemoService.java
+++ collection-demo/src/main/java/com/learn/java/collection/service/ListDemoService.java
@@ -10,15 +10,51 @@ public class ListDemoService {
 
 
 
+	/*
+	 * 0.) Underlying Data Structure: Resizable/Growable Array
+	 * 
+	 * 1.) Insertion Order Preserved
+	 * 2.) Duplicates Allowed
+	 * 3.) NULL insertion is possible
+	 * 4.) No AutoSorting
+	 * 
+	 * 
+	 * */
+	public void arrayListDemo() {
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("A");
+		arrayList.add("B");
+		arrayList.add("D");
+		arrayList.add("C");
+		arrayList.add("F");
+		arrayList.add("W");
+		arrayList.add("Z");
+		arrayList.add("S");
+		arrayList.add("R");
+		arrayList.add("A");
+		System.out.println("Added Array List: "+arrayList);
+		arrayList.add(null);
+		System.out.println("Array List after adding null: "+arrayList);
+		arrayList.add(null);
+		System.out.println("Array List again adding null: "+arrayList);
+		arrayList.add("R");
+		System.out.println("Array List again adding R: "+arrayList);
+		System.out.println("ArrayList Contains R? => "+arrayList.contains("R"));
+		System.out.println("ArrayList Contains null? => "+arrayList.contains(null));		
+		System.out.println("ArrayList Contains index of given object ? => "+arrayList.indexOf(null));
+		System.out.println("ArrayList Contains Object at index 4 ? => "+arrayList.get(4) );
+		System.out.println("Array List removing R: "+arrayList.remove(arrayList.size()-1));
+		System.out.println("Array List set R at null: "+arrayList.set(10, "R"));
+		System.out.println("Array List again adding R: "+arrayList);
+
+	}
+
 	public void linkedListDemo() {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public void arrayListDemo() {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
 
 
 }
