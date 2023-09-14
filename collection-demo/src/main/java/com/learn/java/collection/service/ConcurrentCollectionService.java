1ba39cf1aaf7e7e7564ce627dd8422dbf5d039b8
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-09-14 23:50:36 +0530

    Study-14thSept2023][CollectionDemo] Updated code to test new methods of concurrent hash map.
    Study-14thSept2023][CollectionDemo] Updated code to test new methods of concurrent hash map.

--- collection-demo/src/main/java/com/learn/java/collection/service/ConcurrentCollectionService.java
+++ collection-demo/src/main/java/com/learn/java/collection/service/ConcurrentCollectionService.java
@@ -1,12 +1,41 @@
 package com.learn.java.collection.service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConcurrentCollectionService {
 
 	public void concurrentHashMapDemo() {
-		// TODO Auto-generated method stub
+		
+		//Concurrent hash map new methods
+		
+		ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+		concurrentHashMap.put("1", "A");
+		concurrentHashMap.put("1", "B"); //if key exists, old value will replace with new value 
+		System.out.println("if key exists, old value will replace with new value "+concurrentHashMap);
+		System.out.println();
+		
+		concurrentHashMap.put("2", "C");
+		concurrentHashMap.putIfAbsent("2", "D"); //if key exists, no action performed. skipped
+		System.out.println("if key exists, no action performed. skipped "+concurrentHashMap);
+		System.out.println();
+		
+		concurrentHashMap.replace("2", "D", "C"); //if key and value not match, no action performed
+		System.out.println("if key and value not match, no action performed "+concurrentHashMap);  
+		concurrentHashMap.replace("2", "C", "D"); //if key and value match, replacement happened
+		System.out.println("if key and value match, replacement happened "+concurrentHashMap);
+		System.out.println();
+		
+		concurrentHashMap.remove("2", "C"); //if key and value not match, no action performed
+		System.out.println("if key and value not match, no action performed "+concurrentHashMap);
+		concurrentHashMap.remove("2", "D"); //if key and value match, removed action performed
+		System.out.println("if key and value match, removed action performed "+concurrentHashMap);
+		System.out.println();
+		
+		
+		
 		
 	}
 
