e145e37132b36634950da4ba833d3358feebea5f
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-09-17 11:50:02 +0530

    Study-17thSept2023][Inner-Class] Added code to test method local inner class rules.

--- inner-class-demo/src/main/java/com/example/demo/controller/InnerClassController.java
+++ inner-class-demo/src/main/java/com/example/demo/controller/InnerClassController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.MethodLocalInnerClassService;
 import com.example.demo.service.RegularInnerClassService;
 
 @RestController
@@ -13,10 +14,19 @@ public class InnerClassController {
 
 	@Autowired
 	private RegularInnerClassService regularInnerClassService;
-	
+
+	@Autowired
+	private MethodLocalInnerClassService methodLocalInnerClassService;
+
 	@GetMapping("/regular-inner-class")
 	public String regularInnerClassDemo() {
 		regularInnerClassService.regularInnerClassDemo();
 		return "Regular Inner Class Rules Tested Successfully.";
+	}
+	
+	@GetMapping("/method-local-inner-class")
+	public String methodLocalInnerClassDemo() {
+		methodLocalInnerClassService.methodLocalInnerClassDemo();
+		return "Method Local Inner Class Rules Tested Successfully.";
 	}
 }
