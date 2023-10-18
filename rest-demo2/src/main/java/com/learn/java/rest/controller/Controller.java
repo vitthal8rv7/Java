863d63960ffd9bf6d5460efe321ed7ebc001d2e3
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-10-18 10:04:13 +0530

    [Study-18thOct2023][RESt] Added code to accept multipart files in request.

--- rest-demo2/src/main/java/com/learn/java/rest/controller/Controller.java
+++ rest-demo2/src/main/java/com/learn/java/rest/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.learn.java.rest.controller;
 
+import java.util.Objects;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.learn.java.rest.model.Employee;
 
@@ -16,18 +20,28 @@ public class Controller {
 
 	@GetMapping("/employee/{id}")
 	public Employee getEmployee(@PathVariable String id, @RequestParam String name) {
-		System.out.println("Employee Id: "+id + "\nEmployee name: "+name);
-		//Get Employee Logic
+		System.out.println("Employee Id: " + id + "\nEmployee name: " + name);
+		// Get Employee Logic
 		Employee employee = new Employee(id, name);
 		return employee;
 	}
 
 	@PostMapping("/employee")
 	public Employee setEmployee(@RequestBody Employee employee) {
-		System.out.println("Employee Id: "+employee.getId() + "\nEmployee name: "+employee.getName());
-		//Set Employee Logic
+		System.out.println("Employee Id: " + employee.getId() + "\nEmployee name: " + employee.getName());
+		// Set Employee Logic
 		return employee;
 	}
 
-	
+//	@PostMapping(value= "/employee/multipart", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_OCTET_STREAM_VALUE},
+//    produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value= "/employee/multipart")
+	public Employee setEmployeePicture(@RequestParam(value = "image", required = false) MultipartFile image) {
+		Employee employee = new Employee();
+		System.out.println("Employee Id: " + employee.getId() + "\nEmployee name: " + employee.getName());
+		System.out.println("Is Empty Image: " + Objects.isNull(image));
+		// Set Employee Logic
+		return employee;
+	}
+
 }
