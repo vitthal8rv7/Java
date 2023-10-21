213636524aaf6a50b5ef496bef3385ec8a89fb94
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-10-21 08:52:10 +0530

    [Study-21stOct2023][RESt] Added code to configure bean of Rest Template and set read/response waiting time, connection waiting time.

--- rest-demo1/src/main/java/com/learn/java/rest/config/RestConfig.java
+++ rest-demo1/src/main/java/com/learn/java/rest/config/RestConfig.java
@@ -1,5 +1,8 @@
 package com.learn.java.rest.config;
 
+import java.time.Duration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,4 +15,14 @@ public class RestConfig {
 		RestTemplate restTemplate = new RestTemplate();
 		return restTemplate;
 	}
+	
+	
+	@Bean
+	RestTemplate getRestTemplate(RestTemplateBuilder builder) {
+		return builder
+				.setReadTimeout(Duration.ofMillis(1000))     // Set, how many time unit it will wait for response
+				.setConnectTimeout(Duration.ofMillis(10000)) // Set, how many time unit it will wait for connection
+				.build();
+	}
+
  }
