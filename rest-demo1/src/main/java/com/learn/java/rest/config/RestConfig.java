b6650d201480c804a0b2bf44fd2bec42ef380b4e
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-10-23 16:30:54 +0530

    [Study-23rdOct2023][RESt] Added code test connection and response timeout using rest config.

--- rest-demo1/src/main/java/com/learn/java/rest/config/RestConfig.java
+++ rest-demo1/src/main/java/com/learn/java/rest/config/RestConfig.java
@@ -18,8 +18,8 @@ public class RestConfig {
 
 	@Bean
 	RestTemplate getRestTemplate(RestTemplateBuilder builder) {
-		return builder.setReadTimeout(Duration.ofMillis(1000)) // Set, how many time unit it will wait for response
-				.setConnectTimeout(Duration.ofMillis(10000)) // Set, how many time unit it will wait for connection
+		return builder.setReadTimeout(Duration.ofMillis(300)) // Set, how many time unit it will wait for response
+				.setConnectTimeout(Duration.ofMillis(100)) // Set, how many time unit it will wait for connection
 				.build();
 	}
 
