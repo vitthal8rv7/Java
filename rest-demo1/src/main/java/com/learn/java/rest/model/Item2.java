79fc18f64ef5e3bd06f3745461db70b0c1f7a9ae
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-10-23 15:05:26 +0530

    [Study-23rdOct2023][RESt] Added code to test same endpoint with returning different result by changing Accept header value.

---
+++ rest-demo1/src/main/java/com/learn/java/rest/model/Item2.java
@@ -0,0 +1,23 @@
+package com.learn.java.rest.model;
+
+public class Item2 {
+
+	String item;
+	
+	public Item2() {
+		this.item = this.getClass().getName();
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [item=" + item + "]";
+	}
+}
