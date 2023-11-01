22f18b996ae0a3eea5c92e0f79f7d25a40b7a30b
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-01 20:03:07 +0530

    [Study-1stNov2023][Graphql] Added code to implement graphql Query and Mutation using GraphQL QueryResolver and MutationResolver.

---
+++ graphql-demo/src/main/java/com/learn/java/graphql/GraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.java.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDemoApplication.class, args);
+	}
+
+}
