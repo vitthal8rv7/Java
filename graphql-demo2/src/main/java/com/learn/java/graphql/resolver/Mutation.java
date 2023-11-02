6a74cb109bdf4f45231e4fd85b9a402cd0270b98
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-02 12:55:53 +0530

    [Study-2ndNov2023][Graphql] Implemented and tested GraphQL @SchemaMapping Annotation.

--- graphql-demo2/src/main/java/com/learn/java/graphql/resolver/Mutation.java
+++ graphql-demo2/src/main/java/com/learn/java/graphql/resolver/Mutation.java
@@ -2,6 +2,7 @@ package com.learn.java.graphql.resolver;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import com.learn.java.graphql.model.Employee;
@@ -9,7 +10,8 @@ import com.learn.java.graphql.model.Employee;
 @Controller
 public class Mutation {
 
-	@MutationMapping
+//	@MutationMapping
+	@SchemaMapping(typeName = "Mutation", field = "addEmployee")
 	public Employee addEmployee(@Argument Employee employee) {
 		System.out.println("Id: "+ employee.getId());
 		return employee;
--- graphql-demo2/src/main/java/com/learn/java/graphql/resolver/Query.java
+++ graphql-demo2/src/main/java/com/learn/java/graphql/resolver/Query.java
@@ -2,20 +2,24 @@ package com.learn.java.graphql.resolver;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import com.learn.java.graphql.model.Address;
 import com.learn.java.graphql.model.Employee;
 
 @Controller
 public class Query {
 
-	@QueryMapping
-    public String testQuery() {
+//	@QueryMapping
+	@SchemaMapping(typeName = "Query", field = "")
+	public String testQuery() {
     	System.out.println("Demo 2: ");
     	return "Demo_OK_2";
     }
 	
-	@QueryMapping
+//	@QueryMapping
+	@SchemaMapping(typeName = "Query", field = "getEmployee")
 	public Employee getEmployee(@Argument("id") String id2) {  // here id is field name from schema, and id2 is local name
 												//field name and local name should be same or map by alisa name(@Argument("id"))
 												// if not, we will get null value to the local field or error will occur
@@ -24,4 +28,10 @@ public class Query {
 		System.out.println("Id: "+ id2);
 		return employee;
 	} 
+	
+////	@QueryMapping
+//	@SchemaMapping(typeName = "Address", field = "empAdd")
+//	public Address empAdd(@Argument("addr") Address addr) {  
+//		return addr;
+//	}
 }
