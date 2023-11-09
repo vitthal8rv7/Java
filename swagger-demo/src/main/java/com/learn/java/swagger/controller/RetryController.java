c24612ce644688d6582d154e748d6308fd0e24f7
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-09 07:47:38 +0530

    [Study-9thNov2023][Swagger] Formatted retry controller and service file.

--- swagger-demo/src/main/java/com/learn/java/swagger/controller/RetryController.java
+++ swagger-demo/src/main/java/com/learn/java/swagger/controller/RetryController.java
@@ -14,78 +14,76 @@ public class RetryController {
 
 	@Autowired
 	private RetryService retryService;
-	
+
 	@GetMapping("/retry/test1")
 	String retryService(@RequestParam String data) {
 		System.out.println("\nIn Retry Controller...");
 		String result = null;
-		//try {
-			result = retryService.retryService(data);
-		//} catch(Exception e) {
-		//	System.out.println("Exception e: "+e);
-		//}
+		// try {
+		result = retryService.retryService(data);
+		// } catch(Exception e) {
+		// System.out.println("Exception e: "+e);
+		// }
 		return result;
 	}
-	
+
 	@GetMapping("/retry/customize/test2")
 	String retryService2(@RequestParam String data) {
 		System.out.println("\nIn Retry Controller...");
 		String result = null;
-		//try {
-			result = retryService.retryService2(data);
-		//} catch(Exception e) {
-		//	System.out.println("Exception e: "+e);
-		//}
+		// try {
+		result = retryService.retryService2(data);
+		// } catch(Exception e) {
+		// System.out.println("Exception e: "+e);
+		// }
 		return result;
 	}
-	
-	
+
 	@GetMapping("/retry/customize/test3")
 	String retryService3(@RequestParam String data) {
 		System.out.println("\nIn Retry Controller...");
 		String result = null;
-		//try {
-			result = retryService.retryService3(data);
-		//} catch(Exception e) {
-		//	System.out.println("Exception e: "+e);
-		//}
+		// try {
+		result = retryService.retryService3(data);
+		// } catch(Exception e) {
+		// System.out.println("Exception e: "+e);
+		// }
 		return result;
 	}
-	
-	
+
 	@GetMapping("/retry/customize-spring-properties/test4")
 	String retryService4(@RequestParam String data) {
 		System.out.println("\nIn Retry Controller...");
 		String result = null;
-		//try {
-			result = retryService.retryService4(data);
-		//} catch(Exception e) {
-		//	System.out.println("Exception e: "+e);
-		//}
+		// try {
+		result = retryService.retryService4(data);
+		// } catch(Exception e) {
+		// System.out.println("Exception e: "+e);
+		// }
 		return result;
 	}
-	
+
 	@GetMapping("/retry/customize-retry-template/test5")
 	String retryService5(@RequestParam String data) {
 		System.out.println("\nIn Retry Controller...");
 		String result = null;
-		//try {
-			result = retryService.retryService5(data);
-		//} catch(Exception e) {
-		//	System.out.println("Exception e: "+e);
-		//}
+		// try {
+		result = retryService.retryService5(data);
+		// } catch(Exception e) {
+		// System.out.println("Exception e: "+e);
+		// }
 		return result;
 	}
-	
+
 	@GetMapping("/retry/customize-retry-template-listener/test6")
 	String retryService6(@RequestParam String data) {
 		System.out.println("\nIn Retry Controller...");
 		String result = null;
-		//try {
-			result = retryService.retryService6(data);
-		//} catch(Exception e) {
-		//	System.out.println("Exception e: "+e);
-		//}
+		// try {
+		result = retryService.retryService6(data);
+		// } catch(Exception e) {
+		// System.out.println("Exception e: "+e);
+		// }
 		return result;
 	}
 }
--- swagger-demo/src/main/java/com/learn/java/swagger/service/RetryService.java
+++ swagger-demo/src/main/java/com/learn/java/swagger/service/RetryService.java
@@ -20,10 +20,10 @@ public class RetryService {
 	@Autowired
 	@Qualifier(value = "retryTemplate")
 	private RetryTemplate retryTemplateWithListener2;
-	
+
 	int counter = 1;
 
-	// @Retryable is best when you want to apply @Retryable entire method 
+	// @Retryable is best when you want to apply @Retryable entire method
 	@Retryable
 	public String retryService(String data) {
 		System.out.println("\nIn Retry Service...");
@@ -54,7 +54,7 @@ public class RetryService {
 	}
 
 	// @Retryable is best when you want to apply @Retryable entire method
-	@Retryable(value = {SQLException.class}, maxAttempts = 4, backoff = @Backoff(delay = 100))
+	@Retryable(value = { SQLException.class }, maxAttempts = 4, backoff = @Backoff(delay = 100))
 	public String retryService2(String data) {
 		System.out.println("\nIn Retry Service... & exceptionExpression = \"SQLException.class\"");
 		System.out.println("time: " + System.currentTimeMillis());
@@ -68,9 +68,9 @@ public class RetryService {
 		}
 		return "Operation data: " + data;
 	}
-	
+
 	// @Retryable is best when you want to apply @Retryable entire method
-	@Retryable(value = {NumberFormatException.class}, maxAttempts = 5, backoff = @Backoff(delay = 100))
+	@Retryable(value = { NumberFormatException.class }, maxAttempts = 5, backoff = @Backoff(delay = 100))
 	public String retryService3(String data) {
 		System.out.println("\nIn Retry Service... & exceptionExpression = \"NumberFormatException.class\"");
 		System.out.println("time: " + System.currentTimeMillis());
@@ -84,10 +84,10 @@ public class RetryService {
 		}
 		return "Operation data: " + data;
 	}
-	
+
 	// @Retryable is best when you want to apply @Retryable entire method
-    @Retryable(value = {NumberFormatException.class}, maxAttemptsExpression = "${retry.maxAttempts}",
-            backoff = @Backoff(delayExpression = "${retry.maxDelay}")) 
+	@Retryable(value = {
+			NumberFormatException.class }, maxAttemptsExpression = "${retry.maxAttempts}", backoff = @Backoff(delayExpression = "${retry.maxDelay}"))
 	public String retryService4(String data) {
 		System.out.println("\nIn Retry Service... & exceptionExpression = \"NumberFormatException.class\"");
 		System.out.println("time: " + System.currentTimeMillis());
@@ -102,10 +102,10 @@ public class RetryService {
 		return "Operation data: " + data;
 	}
 
-    public String retryService5(String data) {
+	public String retryService5(String data) {
 		System.out.println("\nIn Retry Service... & retryTemplate");
 		retryTemplate.execute(retryContext -> {
-		    //templateRetryService();
+			// templateRetryService();
 			System.out.println("time: " + System.currentTimeMillis());
 			if (data.length() > 7) {
 				System.out.println("Retry Again...");
@@ -113,18 +113,18 @@ public class RetryService {
 				if (counter > 3) {
 					counter = 1;
 				}
-				System.out.println("retryContext : "+ retryContext);
+				System.out.println("retryContext : " + retryContext);
 				throw new NumberFormatException();
 			}
-		    return "Operation retryContext: " + retryContext;
+			return "Operation retryContext: " + retryContext;
 		});
 		return "Operation data: " + data;
-    }
-    
-    public String retryService6(String data) {
+	}
+
+	public String retryService6(String data) {
 		System.out.println("\nIn Retry Service... & retryTemplate-listener");
 		retryTemplateWithListener2.execute(retryContext -> {
-		    //templateRetryService();
+			// templateRetryService();
 			System.out.println("time: " + System.currentTimeMillis());
 			if (data.length() > 7) {
 				System.out.println("Retry Again...");
@@ -132,11 +132,11 @@ public class RetryService {
 				if (counter > 3) {
 					counter = 1;
 				}
-				System.out.println("retryContext : "+ retryContext);
+				System.out.println("retryContext : " + retryContext);
 				throw new NumberFormatException();
 			}
-		    return "Operation retryContext: " + retryContext;
+			return "Operation retryContext: " + retryContext;
 		});
 		return "Operation data: " + data;
-    }
+	}
 }
