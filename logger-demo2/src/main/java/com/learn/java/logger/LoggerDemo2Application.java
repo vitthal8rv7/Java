fff28b95dd8f45c51a345b7f6470cc2b73d86c38
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-10 12:59:13 +0530

    [Study-10thNov2023][Logger] Added code to set up console logger configurations by using logback.xml file.

---
+++ logger-demo2/src/main/java/com/learn/java/logger/LoggerDemo2Application.java
@@ -0,0 +1,13 @@
+package com.learn.java.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerDemo2Application.class, args);
+	}
+
+}
