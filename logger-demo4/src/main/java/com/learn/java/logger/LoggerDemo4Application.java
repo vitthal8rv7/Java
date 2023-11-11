143b0be00f084e1cf953f97373e03d42424afeeb
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-11 12:29:55 +0530

    [Study-11thNov2023][Logger] Configure log levels and log pattern and save to log file.

---
+++ logger-demo4/src/main/java/com/learn/java/logger/LoggerDemo4Application.java
@@ -0,0 +1,13 @@
+package com.learn.java.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerDemo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerDemo4Application.class, args);
+	}
+
+}
