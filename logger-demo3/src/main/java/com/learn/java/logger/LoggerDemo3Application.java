8d2c8f4e9eeb2e0039bf0c4cccca653a319656b1
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-11 11:28:23 +0530

    [Study-10thNov2023][Logger] Configure log level and logging pattern in application properties file.

---
+++ logger-demo3/src/main/java/com/learn/java/logger/LoggerDemo3Application.java
@@ -0,0 +1,13 @@
+package com.learn.java.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerDemo3Application.class, args);
+	}
+
+}
