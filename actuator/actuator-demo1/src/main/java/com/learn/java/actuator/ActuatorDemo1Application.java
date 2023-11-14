e542cade62d1b86c762df0f9f29e039b17bec6d8
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-11-14 17:31:27 +0530

    [Study-14thNov2023][Actuator] Added code to test actuator.

---
+++ actuator/actuator-demo1/src/main/java/com/learn/java/actuator/ActuatorDemo1Application.java
@@ -0,0 +1,13 @@
+package com.learn.java.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDemo1Application.class, args);
+	}
+
+}
