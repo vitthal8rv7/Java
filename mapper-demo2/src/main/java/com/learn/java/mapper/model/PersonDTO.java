7952f0681ab692ed5f11b428acf49f30664a1713
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-12-02 06:29:25 +0530

    Study-2ndDec2023][Mapper] Added code to solve complex mapping. not working.

--- mapper-demo2/src/main/java/com/learn/java/mapper/model/PersonDTO.java
+++ mapper-demo2/src/main/java/com/learn/java/mapper/model/PersonDTO.java
@@ -3,22 +3,37 @@ package com.learn.java.mapper.model;
 import java.io.Serializable;
 
 import com.googlecode.jmapper.annotations.JMap;
-import com.googlecode.jmapper.annotations.JMapAccessor;
+import com.googlecode.jmapper.annotations.JMapConversion;
 
 public class PersonDTO implements Serializable {
 
 	private static final long serialVersionUID = -6245047560637013213L;
 
-	@JMap(attributes = {"lastName", "firstName"}, classes = {Person.class, Person.class })
+	@JMap(attributes = { "lastName", "firstName" }, classes = { Person.class, Person.class })
 	private String fullName;
+
+	@JMapConversion(from = { "firstName",  "lastName"}, to = { "fullName" })
+	public String conversion(String firstName, String lastName) {
+		System.out.println(firstName);
+		System.out.println(lastName);
+		return firstName+lastName;
+	}
 
 	@JMap("address")
 	private Address address;
-	
-	//@JMap(value = "address.city", classes = Address.class)
-	@JMapAccessor(name = "address.city", get = "address.getCity", classes = Address.class)
+
+	// @JMap(value = "address.city", classes = Address.class)
+	// @JMapAccessor(name = "address.city", get = "address.getCity", classes =
+	// Address.class)
+
+	@JMap(value = "address.city", classes = Address.class)
 	private String city;
 
+//	@JMapConversion(from = { "address.city" }, to = { "city" })
+//	public String conversion(String city) {
+//		return city;
+//	}
+	
 	public String getFullName() {
 		return fullName;
 	}
@@ -35,7 +50,6 @@ public class PersonDTO implements Serializable {
 		this.city = city;
 	}
 
-	
 	public Address getAddress() {
 		return address;
 	}
@@ -43,6 +57,5 @@ public class PersonDTO implements Serializable {
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-
 
 }
