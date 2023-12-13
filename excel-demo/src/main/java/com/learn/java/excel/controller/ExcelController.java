74db801890019b4b59e50e4a481d6d818ad91532
Vitthal Aradwad <vitthalaradwad@gmail.com>
2023-12-13 19:54:35 +0530

    [Study-13thDec2023][Excel] Added code get excel file name in API request and print it on console.

--- excel-demo/src/main/java/com/learn/java/excel/controller/ExcelController.java
+++ excel-demo/src/main/java/com/learn/java/excel/controller/ExcelController.java
@@ -3,9 +3,12 @@ package com.learn.java.excel.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learn.java.excel.service.ExcelService;
+
+import io.micrometer.common.util.StringUtils;
 
 @RestController
 @RequestMapping("/excel/demo")
@@ -15,14 +18,18 @@ public class ExcelController {
 	public ExcelService excelService;
 
 	@GetMapping("/read/old/excel/file")
-	public void readOldExcelFile() {
-		String file = "file1.xls";
+	public void readOldExcelFile(@RequestParam String file) {
+		if (StringUtils.isBlank(file)) {
+			file = "file1.xls";
+		}
 		excelService.readOldFile(file);
 	}
 
 	@GetMapping("/read/excel/file")
-	public void readExcelFile() {
-		String file = "file2.xlsx";
+	public void readExcelFile(@RequestParam String file) {
+		if (StringUtils.isBlank(file)) {
+			file = "file2.xlsx";
+		}
 		excelService.readFile(file);
 	}
 }
