414b1f411f3cf70fe3383e6c46b5e6586fd050ef
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-02-07 12:19:44 +0530

    [Study-7thFeb2024][Kafka] Added code to publish message and get the result using completable future.

--- kafka-demo/src/main/java/com/learn/java/kafka/controller/KafkaController.java
+++ kafka-demo/src/main/java/com/learn/java/kafka/controller/KafkaController.java
@@ -1,7 +1,10 @@
 package com.learn.java.kafka.controller;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,27 +15,41 @@ import com.learn.java.kafka.model.User;
 @RestController
 @RequestMapping("/kafka/demo")
 public class KafkaController {
-	
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
 
-    @PostMapping("/produce")
-    public String produceMessage(@RequestBody String message) {
-        kafkaTemplate.send("test-topic", message);
-        return "Message sent: " + message;
-    }
-    
-    @PostMapping("/produce/user")
-    public String produceMessage(@RequestBody User user) {
-        kafkaTemplate.send("test-topic", user.toString());
-        return "Message sent: " + user;
-    }
-    
-    @PostMapping("/produce/programatically-created-topic")
-    public String produceMessageToCustomTopic(@RequestBody String message) {
-        kafkaTemplate.send("myTopic123", message);
-        System.out.println("Inside produceMessageToCustomTopic: "+message);
-        return "Message sent: " + message;
-    }
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@PostMapping("/produce")
+	public String produceMessage(@RequestBody String message) {
+		kafkaTemplate.send("test-topic", message);
+		return "Message sent: " + message;
+	}
+
+	@PostMapping("/produce/user")
+	public String produceMessage(@RequestBody User user) {
+		kafkaTemplate.send("test-topic", user.toString());
+		return "Message sent: " + user;
+	}
+
+	@PostMapping("/produce/programatically-created-topic")
+	public String produceMessageToCustomTopic(@RequestBody String message) {
+		kafkaTemplate.send("myTopic123", message);
+		System.out.println("Inside produceMessageToCustomTopic: " + message);
+		return "Message sent: " + message;
+	}
+
+	@PostMapping("/produce/new-message")
+	public String produceNewMessage(@RequestBody String message) {
+		CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("test-topic", message);
+		future.whenComplete((result, exception) -> {
+			if (exception == null) {
+				System.out.println(
+						"Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+			} else {
+				System.out.println("Unable to send message=[" + message + "] due to : " + exception.getMessage());
+			}
+		});
+		return "Message sent: " + message;
+	}
 
 }
