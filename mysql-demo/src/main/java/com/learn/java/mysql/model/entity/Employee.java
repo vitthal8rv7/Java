f5fcdbf59134f6f1aeb774ddefd1a3722407fdee
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-03-14 03:19:00 +0530

    [Study-14thMarch2024][MySQL] Added code to test set custom name to foreign key  (using @JoinColumn).

--- mysql-demo/src/main/java/com/learn/java/mysql/model/entity/Employee.java
+++ mysql-demo/src/main/java/com/learn/java/mysql/model/entity/Employee.java
@@ -9,13 +9,12 @@ import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -91,5 +90,6 @@ public class Employee {
  	private String gender;
 	
 	@ManyToOne
+	@JoinColumn(name = "dp_id")
 	private Department department;
 }
