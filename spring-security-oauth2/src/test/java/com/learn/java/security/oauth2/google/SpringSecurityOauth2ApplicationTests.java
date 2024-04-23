27bc02ca0092b2b0dca0e5411b56faf13d115e62
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-04-23 23:21:37 +0530

    [Study-23rddApril2024][Spring-Security] Added code to test oauth2 service.

---
+++ spring-security-oauth2/src/test/java/com/learn/java/security/oauth2/google/SpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java.security.oauth2.google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
