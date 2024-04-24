e61ebebe7d53b74748f7bff98d4a3981d8e87ec7
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-04-24 09:37:22 +0530

    [Study-24thApril2024][Spring-Security] Updated code to test oauth2 service. After OAuth2 authentication, Used session for further communication between User/Client(you application).

--- spring-security-oauth2/src/main/java/com/learn/java/security/oauth2/google/controller/AuthController.java
+++ spring-security-oauth2/src/main/java/com/learn/java/security/oauth2/google/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.learn.java.security.oauth2.google.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
-	
-	@GetMapping("/home")
-	public String homeOAuth2() {
-		LOGGER.info("OAuth 2 tested successfully.");
+
+	@GetMapping("")
+	public String authenticated(Authentication authentication) {
+		try {
+
+//			LOGGER.info("OAuth 2 tested successfully." + authentication.getPrincipal());
+			System.out.println("0");
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			System.out.println("1");
+			LOGGER.info("oauth : " + auth.toString());
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
 		return "OAuth 2 tested successfully.";
 	}
 
-	@GetMapping("/ram")
+	@GetMapping("/home")
+	public String homeOAuth2() {
+		LOGGER.info("OAuth 2 tested successfully.");
+		return "OAuth 2 tested successfully.2";
+	}
+
+	@GetMapping("/home/ram")
 	public String ramOAuth2() {
 		LOGGER.info("OAuth 2 tested successfully.");
-		return "OAuth 2 tested successfully......";
+		return "OAuth 2 tested successfully......3";
 	}
 
 }
