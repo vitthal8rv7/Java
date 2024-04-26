a7a8db4990944e46558b0c144f5a31d66b658ab8
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-04-26 16:27:03 +0530

    [Study-26thApril2024][Spring-Security] Updated security config.

--- spring-security-oauth2-client/src/main/java/com/learn/java/security/oauth2/client/config/SecurityConfig.java
+++ spring-security-oauth2-client/src/main/java/com/learn/java/security/oauth2/client/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.learn.java.security.oauth2.client.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,9 +15,9 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests(request -> {
-			request.requestMatchers("/", "/registerUser/**", "/home").permitAll();
-			request.anyRequest().authenticated();
+		http.authorizeHttpRequests(authorize -> {
+			authorize.requestMatchers("/", "/registerUser/**", "/home").permitAll();
+			authorize.anyRequest().authenticated();
 		});
 
 		http.sessionManagement(session -> {
@@ -28,10 +29,8 @@ public class SecurityConfig {
 					.expiredUrl("/expiredUrl.html");
 
 		});
-
-		http.oauth2Login(oauthCustomizer -> {
-				System.out.println("oauthCustomizer"+ oauthCustomizer.toString());
-				});
+		http.oauth2Client(Customizer.withDefaults());
+		http.oauth2Login(Customizer.withDefaults());
 		return http.build();
 	}
 
