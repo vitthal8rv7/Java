d6fe5496e8f90e9c3fd61dd11d72c37d27fd20ab
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-05-03 11:39:33 +0530

    [Study-3rdMay2024][Spring-DSA] Added code solve DSA example 1.

---
+++ spring-dsa/src/test/java/com/learn/java/dsa/SpringDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
