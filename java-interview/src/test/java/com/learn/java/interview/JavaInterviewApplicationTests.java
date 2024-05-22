155389a5111294ec3fd11a76cceeb54ca296a6a4
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-05-22 08:06:49 +0530

    [Study-22ndMay2024][Interview] Added questions like How to replace a specific string in a text file?

--- java-interview/src/test/java/com/learn/java/interview/JavaInterviewApplicationTests.java
+++ java-interview/src/test/java/com/learn/java/interview/JavaInterviewApplicationTests.java
@@ -1,5 +1,13 @@
 package com.learn.java.interview;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -12,10 +20,65 @@ import java.util.stream.Collectors;
 
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.Environment;
 
 //@SpringBootTest
 class JavaInterviewApplicationTests {
+	@Test
+	void test7() {
+	}
 
+//	@Test
+//	void test6() {
+//		 
+//		
+//		FileWriter fw = null;
+//		File file = new File("./file1.txt");
+//		BufferedReader br = null;
+//		String line = null;
+//		String newData = "";
+//		String oldReplacementString = "85";
+//		String newReplacementString = "95";	
+//		try {
+//			// Read File
+//			br = new BufferedReader(new FileReader(file));
+//			do {
+//				line = br.readLine();
+//				if(line == null) {
+//					break;
+//				}
+//				newData = newData + line + System.lineSeparator();
+//			}while(line!=null);
+//			newData = newData.substring(0, newData.length()-1);
+//			
+//			// Replace specific string from the data 
+//			System.out.println("oldData: "+ newData);
+//			newData = newData.replaceAll(oldReplacementString, newReplacementString);
+//			
+//			// Write data to the File
+//			fw = new FileWriter(file);
+//			fw.write(newData);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				br.close();
+//				fw.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		System.out.println("newData: "+ newData);
+//
+//	}
+	
+	
+//	@Test
+//	void test6()
+//	{
+//		String input = "Hello World, this is a Java program!";
+//		System.out.println(input.replaceAll("[aeiouAEIOU]", ""));
+//	}
 
 //	@Test
 //	void test5() {
