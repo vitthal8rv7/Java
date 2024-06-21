b9e68f75ce4d9217a5789a11fc14add0078e8081
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-06-21 17:34:56 +0530

    [Study-21stJune2024][Interview] Added interview questions answers on Actuator Q. What is Spring Boot Actuator? Q, List All Core Endpoints?

--- actuator/actuator-demo1/src/main/java/com/learn/java/actuator/ActuatorDemo1Application.java
+++ actuator/actuator-demo1/src/main/java/com/learn/java/actuator/ActuatorDemo1Application.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class ActuatorDemo1Application {
 
 	public static void main(String[] args) {
