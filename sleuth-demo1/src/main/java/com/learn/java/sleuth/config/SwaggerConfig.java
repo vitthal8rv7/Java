a984859caac0904942f664328879eb357b077834
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-06-24 08:32:14 +0530

    [Study-24thJune2024][Interview] Added code to propagate trace id to other microservice using micrometer.

--- sleuth-demo1/src/main/java/com/learn/java/sleuth/config/SwaggerConfig.java
+++ sleuth-demo1/src/main/java/com/learn/java/sleuth/config/SwaggerConfig.java
@@ -1,92 +1,24 @@
-//package com.learn.java.sleuth.config;
-//
-//import java.lang.reflect.Field;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//import org.springframework.beans.BeansException;
-//import org.springframework.beans.factory.config.BeanPostProcessor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.ReflectionUtils;
-//import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
-//
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.builders.ResponseBuilder;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.service.Response;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
-//
-//@Configuration
-//@Component
-//public class SwaggerConfig {
-//
-//	@Bean
-//	Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).apiInfo(apiInfo())
-//				.globalResponses(HttpMethod.GET, list()) // common and apply to all APIs.
-//				.select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
-//	}
-//
-//	private List<Response> list() {
-//		List<Response> list = new ArrayList<Response>();
-//		list.add(new ResponseBuilder().code("500").description("500 message").build());
-//		list.add(new ResponseBuilder().code("403").description("Forbidden!!!!!").build());
-//		return list;
-//	}
-//
-//	private ApiInfo apiInfo() {
-//		return new ApiInfoBuilder().title("Java Learning REST API ")
-//				.description("Spring Boot REST API for Swagger Demo Application").version("1.0.0")
-//				.termsOfServiceUrl("http://localhost:8080/swagger-ui/index.html")
-//				.contact(new Contact("Vitthal Aradwad", "https://www.google.com", "vitthalaradwad@gmail.com"))
-//				.license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-//				.build();
-//	}
-//
-////    Exception: Problem
-////    Caused by: java.lang.NullPointerException: Cannot invoke "org.springframework.web.servlet.mvc.condition.PatternsRequestCondition.getPatterns()" because "this.condition" is null
-////	at springfox.documentation.spring.web.WebMvcPatternsRequestConditionWrapper.getPatterns(WebMvcPatternsRequestConditionWrapper.java:56) ~[springfox-spring-webmvc-3.0.0.jar:3.0.0]
-//	// Solution1: @EnableWebMvc
-//	// Solution2: below bean
-//	@Bean
-//	public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-//		return new BeanPostProcessor() {
-//
-//			@Override
-//			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-//				if (bean instanceof WebMvcRequestHandlerProvider) {
-//					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
-//				}
-//				return bean;
-//			}
-//
-//			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(
-//					List<T> mappings) {
-//				List<T> copy = mappings.stream().filter(mapping -> mapping.getPatternParser() == null)
-//						.collect(Collectors.toList());
-//				mappings.clear();
-//				mappings.addAll(copy);
-//			}
-//
-//			@SuppressWarnings("unchecked")
-//			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
-//				try {
-//					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
-//					field.setAccessible(true);
-//					return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
-//				} catch (IllegalArgumentException | IllegalAccessException e) {
-//					throw new IllegalStateException(e);
-//				}
-//			}
-//		};
-//	}
-//}
+package com.learn.java.sleuth.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+     GroupedOpenApi publicOpenAPI() {
+        return GroupedOpenApi.builder().group("public-apis").pathsToMatch("/**").build();
+    }
+    
+    @Bean
+    OpenAPI customOpenApi() {
+    	return new OpenAPI().info(new Info().title("T1").version("V1"));
+    }
+    
+}
--- sleuth-demo1/src/main/java/com/learn/java/sleuth/controller/SleuthController.java
+++ sleuth-demo1/src/main/java/com/learn/java/sleuth/controller/SleuthController.java
@@ -2,18 +2,20 @@ package com.learn.java.sleuth.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.learn.java.sleuth.model.Employee;
+import brave.Span;
+import brave.Tracer;
+
+
 
 @RestController
 @RequestMapping("/logger/demo")
@@ -21,77 +23,25 @@ public class SleuthController {
 
 	private static Logger logger = LoggerFactory.getLogger(SleuthController.class);
 
-//	@Autowired
-//	private Tracer tracer;
+	@Autowired
+	private Tracer tracer;
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@GetMapping("/log-levels")
 	public String checkLogLevels() {
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getForObject("http://localhost:8081/logger/demo/log-levels", String.class);
-		logger.info("Log Level : INFO");
-		logger.trace("Log Level : TRACE");
-		logger.warn("Log Level : WARN");
-		logger.debug("Log Level : DEBUG");
-		logger.error("Log Level : ERROR");
-		logAmplify();
+		
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("X-TRACE-OPERATION", "log-levels");
+        headers.set("X-TRACE-REQUESTID", tracer.currentSpan().context().traceIdString());
+        headers.set("traceId", ""+tracer.currentSpan().context().traceId());
+        headers.set("X-TRACE-USER", "Vitthal Aradwad");
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/logger/demo/traceid/2", HttpMethod.GET,
+                httpEntity, String.class);
+        logger.info("httpEntity"+ responseEntity.getBody());
 		return "Log Level Checked.\n Default Log Level Is INFO.";
 	}
-
-	private void logAmplify() {
-		logger.info("Log Level : INFO");
-		logger.trace("Log Level : TRACE");
-		logger.warn("Log Level : WARN");
-		logger.debug("Log Level : DEBUG");
-		logger.error("Log Level : ERROR");
-	}
-
-	@GetMapping("/employee")
-	public Employee getEmployee(@RequestParam String employeeId) {
-		logger.info("Inside get employee");
-		Employee employee = new Employee();
-		employee.setId(employeeId);
-		employee.setName("E-" + employeeId);
-		return employee;
-	}
-
-	@PostMapping("/employee")
-	public Employee addEmployee(@RequestBody Employee employee) {
-		logger.info("Inside add employee");
-		return employee;
-	}
-
-	@PutMapping("/employee")
-	public Employee updateEmployee(@RequestBody Employee employee) {
-		logger.info("Inside update employee");
-		return employee;
-	}
-
-	@PatchMapping("/employee")
-	public Employee updateEmployeeName(@RequestParam String employeeId, @RequestParam String employeeName) {
-		logger.info("Inside update employee name");
-		Employee employee = new Employee();
-		employee.setId(employeeId);
-		employee.setName(employeeName);
-		return employee;
-	}
-
-	@DeleteMapping("/employee")
-	public Boolean deleteEmployee(@RequestParam String employeeId) {
-		logger.info("Inside delete employee");
-		return true;
-	}
-
-//	@GetMapping("/traceid")
-//	public String getSleuthTraceId() {
-//		logger.info("Hello with Sleuth");
-//		Span span = tracer.currentSpan();
-//		if (span != null) {
-//			logger.info("Span ID hex {}", span.context().spanIdString());
-//			logger.info("Span ID decimal {}", span.context().spanId());
-//			logger.info("Trace ID hex {}", span.context().traceIdString());
-//			logger.info("Trace ID decimal {}", span.context().traceId());
-//		}
-//		return "Hello from Sleuth";
-//	}
-
 }
