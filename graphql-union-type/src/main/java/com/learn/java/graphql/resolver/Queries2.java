f35d3988406c366a57a5618838f45ebe2a3e15b5
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-07-25 16:02:12 +0530

    [Study-25thJuly2024][Interview] Added interview questions answers on Graphql. Q.) Added code for Union in graphql?

---
+++ graphql-union-type/src/main/java/com/learn/java/graphql/resolver/Queries2.java
@@ -0,0 +1,60 @@
+package com.learn.java.graphql.resolver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import com.learn.java.graphql.model.Animal;
+import com.learn.java.graphql.model.Cat;
+import com.learn.java.graphql.model.Dog;
+import com.learn.java.graphql.model.Post;
+import com.learn.java.graphql.model.User;
+
+@Controller
+public class Queries2 {
+
+	@QueryMapping
+	public List<Object> search(@Argument(name = "keyword") String keyword) {
+		String lowerKeyword = keyword.toLowerCase();
+		List<Object> results = new ArrayList<>();
+		final List<User> users = Arrays.asList(new User("1", "John Doe"), new User("2", "Jane Doe"));
+
+		final List<Post> posts = Arrays.asList(
+				new Post("1", "GraphQL Introduction", "GraphQL is a query language for APIs."),
+				new Post("2", "Spring Boot with GraphQL", "How to use GraphQL with Spring Boot."));
+		users.stream()
+			.filter(user -> user.getName().toLowerCase().contains(lowerKeyword))
+			.forEach(results::add);
+		posts.stream()
+			.filter(post -> post.getTitle().toLowerCase().contains(lowerKeyword)
+				|| post.getContent().toLowerCase().contains(lowerKeyword))
+			.forEach(results::add);
+
+		return results;
+	}
+//	
+//	@SchemaMapping(typeName = "resolveType") 
+//    public Class<?> resolveType(Object searchResult) {
+//        if (searchResult instanceof User) {
+//            return User.class;
+//        } else if (searchResult instanceof Post) {
+//            return Post.class;
+//        }
+//        return null;
+//    }
+	
+    @SchemaMapping(typeName = "searchResult")   // anything but not "SearchResult", it is type define for union
+    public Class<?> resolveType(Object result) {
+        if (result instanceof User) {
+            return User.class;
+        } else if (result instanceof Post) {
+            return Post.class;
+        }
+        return null;
+    }
+}
