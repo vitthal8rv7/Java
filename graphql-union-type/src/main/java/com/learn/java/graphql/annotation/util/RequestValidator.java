2ee2486b8e05533e0da1d446da61ed01dfefd2ca
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-08-02 08:40:06 +0530

    [Study-2ndAugust2024][Interview] Added interview questions answers on Annotations. Q.) How to create custom annotations.

---
+++ graphql-union-type/src/main/java/com/learn/java/graphql/annotation/util/RequestValidator.java
@@ -0,0 +1,16 @@
+package com.learn.java.graphql.annotation.util;
+
+import com.learn.java.graphql.annotation.ValidateRequest;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class RequestValidator implements ConstraintValidator<ValidateRequest,String>{
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		System.out.println("Inside is valid");
+		return true;
+	}
+
+}
