0f375275fe29c84e0f72c8e818db913086df2573
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-08-12 12:12:16 +0530

    [Study-12thAugust2024][Interview] Added code to test custom spring security provider with different profiles.

---
+++ security-demo-1/src/test/java/com/java/learning/security/SecurityDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.learning.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
