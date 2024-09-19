94e718d24fd0185d2ab1257e63880c919e791169
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-09-19 10:24:01 +0530

    [Study-19thSept2024][Interview] Added Testing, Security basic information for interview revision.

--- junit5-demo/src/main/java/com/learn/java/junit5/Junit5DemoApplication.java
+++ junit5-demo/src/main/java/com/learn/java/junit5/Junit5DemoApplication.java
@@ -2,6 +2,7 @@ package com.learn.java.junit5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class Junit5DemoApplication {
