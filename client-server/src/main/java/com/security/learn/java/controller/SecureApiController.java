01d2b0e8686be902afc8ba4f1325dbcc2706e1e5
Vitthal Aradwad <vitthalaradwad@gmail.com>
2024-09-21 16:20:03 +0530

    [Study-21stSept2024][Interview] updated code to develop client server(app).

---
+++ client-server/src/main/java/com/security/learn/java/controller/SecureApiController.java
@@ -0,0 +1,30 @@
+package com.security.learn.java.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecureApiController {
+    @GetMapping("/")
+    public String demo(Authentication authentication) {
+//        return "demo";
+//        if(authentication instanceof UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken){
+//            System.out.println(usernamePasswordAuthenticationToken);
+//        } else if (authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+//            System.out.println(oAuth2AuthenticationToken);
+//        }
+        return "secure.html";
+    }
+
+    @GetMapping("/public")
+    public String publicApi(Authentication authentication) {
+        return "public api";
+    }
+    @GetMapping("/private")
+    public String privateApi(Authentication authentication) {
+        return "private api";
+    }
+}
